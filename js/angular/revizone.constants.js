b6a39e0e1ac89f7328a10711907ca81d5a950fed
aureleoules <aurele@oules.com>
2017-01-16 18:23:48 +0100

    fixed angular constants.js

---
+++ js/angular/revizone.constants.js
@@ -0,0 +1,10 @@
+angular.module('revizone')
+
+.constant('AUTH_EVENTS', {
+  notAuthenticated: 'auth-not-authenticated'
+})
+
+.constant('API_ENDPOINT', {
+  url: 'http://www.revizone.fr:8089/api'
+  //  For a simulator use: url: 'http://127.0.0.1:8080/api'
+});
