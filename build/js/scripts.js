6b84c3a6b64f8688990cf88ef3a60e0899fa0c74
Aurèle Oulès <aurele@oules.com>
2017-01-31 02:00:29 +0100

    grunted

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1682,7 +1682,7 @@ angular.module('revizone')
 })
 
 .constant('API_ENDPOINT', {
-  url: 'https://localhost:8089/api'
+  url: 'https://www.revizone.fr:8089/api'
   //  For a simulator use: url: 'http://127.0.0.1:8080/api'
 });
 
